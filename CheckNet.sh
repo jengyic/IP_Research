91223507cfefca4fa293d545008b460a81779864
morrisc <JengYi.Chen@gmail.com>
2014-08-07 21:50:15 +0800

    Updat CheckNet.sh

--- CheckNet.sh
+++ CheckNet.sh
@@ -8,7 +8,8 @@ while [ 1 ]
 do
 	RHOSTALIVE=`ping -q -c 1 -W 2 ${RHOST} > /dev/null && echo 1`
 	if [ ! -n "${RHOSTALIVE}" ]||[ ${RHOSTALIVE} -ne 1 ]; then
-		echo "Network Error. Please check it."
+		#echo "Network Error. Please check it."
+		BAD=1
 	else
 		#echo "Ping to $RHOST is fine."
 		exit 0
